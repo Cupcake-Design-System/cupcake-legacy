d0a2daae01d8da52df0f240c13d31eea40a72769
Josh Easter <josh.easter@ipreo.com>
2017-01-17 13:14:03 -0500

    Merge

--- README.md
+++ README.md
@@ -1,58 +1,115 @@
-<img alt="cupcake-logo" src="extra/cupcake-horizontal-logo.png" width="350">
+<img alt="cupcake-logo" src="src/img/cupcake-horizontal-logo.png" width="350">
 
-Project Cupcake is the internal Ipreo design system that brings together UX guidelines, an HTML/CSS framework, and Angular 2 components. This repository includes everything you need to build, customize, test, and deploy Cupcake. For complete documentation, visit the Cupcake website.
 
-### Features
-* **Feature A**: Lorem Ipsum.
-* **Feature B**: Lorem Ipsum.
+Project Cupcake is the Ipreo design system that brings together UX guidelines, an HTML/CSS framework, and rich ui components. This repository includes everything you need to build, customize, test, and deploy Cupcake. For complete documentation, visit the Cupcake documentation site.
+
 
 ## Getting Started
 
-Cupcake is currently published as two npm packages:
+1. If needed, [install](http://blog.nodeknockout.com/post/65463770933/how-to-install-node-js-and-npm) `node` and `npm` (Node Package Manager).
+- If needed, install `gulp` with `npm install gulp -g`.
+- Clone this repo with `git clone gitaddress` or download the zip.
+- In terminal, `cd` to the folder containing your project. Alternatively, you can type `cd ` and drag the location of the folder into your terminal and hit enter (on Macs).
+- In terminal, type `npm install`. If (and _only_ if) `npm install` isn't working, try `sudo npm install`. This should install all [dependencies](#dependencies).
+- In terminal, enter `gulp`.
+- Your browser should open at `http://localhost:3000`. You can access this same page on any device on the same wifi network and they'll see whats on your screen. It'll even sync scrolls and clicks!
+- Edit your code inside of the `src` folder.
+- Your complied and minified css, html, and javascript files will be created and updated in `dist/`. Never edit files within the `dist/` folder, as it gets deleted frequently.
+- Keep `gulp` running while you're making changes. When you want to stop the gulp task, hit `ctrl + C`.
 
-* __cupcake-core.__ Contains the static styles for building HTML components.
-* __cupcake-angular.__ Contains the Angular 2 components. This package depends on cupcake-core for styles.
+_For theming: Running 'gulp theme' will prompt you for a name. This name will be used to make your .scss file inside theme folder. The generated file will be properly linked to dependencies and all you to easily change the root variables._
 
+## Requirements
+- Node/NPM
+- LibSass
+- Gulp
 
-### Installing Cupcake Core
+## Features
+- Live reloading with BrowserSync
+- Github Pages deployment
+- Sass compilation and linting
+- Autoprefixer
 
-1. Install Cupcake-core package through npm:
-    ```bash
-    npm install cupcake --save
-    ```
+## Current Dependencies
+```
+    "browser-sync": "^2.0.0-rc4",
+    "colors": "^1.1.2",
+    "del": "^2.0.2",
+    "file-exists": "^2.0.0",
+    "gulp": "^3.9.0",
+    "gulp-autoprefixer": "^3.1.1",
+    "gulp-clean-css": "^2.0.7",
+    "gulp-gh-pages": "^0.5.4",
+    "gulp-notify": "^2.2.0",
+    "gulp-plumber": "^1.1.0",
+    "gulp-prompt": "^0.2.0",
+    "gulp-rename": "^1.2.0",
+    "gulp-sass": "^3.1.0",
+    "gulp-stylelint": "^3.7.0",
+    "run-sequence": "^1.2.2",
+    "stylelint-config-standard": "^15.0.1",
+    "stylelint-declaration-use-variable": "^1.6.0",
+    "stylelint-scss": "^1.4.1",    
+    "gulp-sourcemaps": "^2.4.0",
+    "vinyl-paths": "^2.0.0"
+```
 
-2. Include the cupcake.min.css in your HTML file:
-    ```html
-    <link rel="stylesheet" href="path/to/node_modules/cupcake/cupcake-core.min.css">
-    ```
+## Tasks
+- clean:dist
+- styles
+- styles:prod
+- browser-sync
+- deploy
+- lint
+- watch
+- stats
+- themes
+- default
+  - clean:dist
+  - html
+  - styles
+  - themes
+  - copy
+  - styles:prod
+  - browser-sync
+  - watch
+- build
+  - clean:dist
+  - html
+  - styles
+  - themes
+  - copy
+  - styles:prod
+- audit *in-progress
+  - lint
+  - stats
 
-    If your site is built with [angular-cli](https://github.com/angular/angular-cli), you can achieve the above by adding the file to the styles array in 
-    `angular-cli.json`:
-    ```js
-    "styles": [
-        ...
-        "../node_modules/cupcake/cupcake-core.min.css"
-        ...
-    ]
-    ```
+## Directory structure
 
-3. Write your HTML with the Cupcake CSS class names and markup.
+```
+┌── .gitignore
+├── .htaccess
+├── .stylelintrc.json
+├── src
+│   ├── img
+│   ├── index.html
+│   ├── fonts
+│   ├── themes
+│   └── scss
+│       ├──── base
+│       ├──── config
+│       ├──── forms
+│       ├──── grid
+│       ├──── layouts
+│       ├──── modules
+│       ├──── navigation
+│       ├──── ui
+│       ├──── utility
+│       └── default.scss
+├── gulpfile.js
+└── package.json
+```
 
-## Documentation
+## Bugs & Support
+Please list all bugs and feature requests in the Github issue tracker.
 
-For full documentation on the Cupcake Design System, including a list of components and example usage, see [our website](link).
-
-## Contributing
-
-The Cupacake team welcomes & encourages contributions. For more detailed information, see [CONTRIBUTING.md](link).
-
-
-## Feedback 
-
-If you find a bug or want to request a new feature, please open a [GitHub issue](link).
-If possible please provide a minimal demo illustrating the issue with https://codepen.io
-- Fork the Cupcake [codepen template](link) here and submit your link with the issue.
-- 
-### Community
-**Slack:** Join the cupcake channel here: #.  
-**Email:** @ .  
---
+++ gulpfile.js
@@ -0,0 +1,215 @@
+var gulp          = require('gulp'),
+    browserSync   = require('browser-sync'),
+    prefix        = require('gulp-autoprefixer'),
+    cleanCSS      = require('gulp-clean-css'),
+    deploy        = require('gulp-gh-pages'),    
+    notify        = require('gulp-notify'),
+    plumber       = require('gulp-plumber'),
+    prompt        = require('gulp-prompt'), 
+    rename        = require('gulp-rename'),
+    sass          = require('gulp-sass'),
+    sourcemaps    = require('gulp-sourcemaps'),
+    stylelint     = require('gulp-stylelint'), 
+    runSequence   = require('run-sequence'),
+    reload        = browserSync.reload,
+    del           = require('del'),
+    vinylPaths    = require('vinyl-paths'),
+    colors        = require('colors'),
+    fileExists    = require('file-exists');
+    download      = require('gulp-downloader');    
+
+var bases = {
+    app:  'src/',
+    dist: 'dist/',
+};
+
+colors.setTheme({
+  silly:   'rainbow',
+  input:   'grey',
+  verbose: 'cyan',
+  prompt:  'grey',
+  info:    'green',
+  data:    'grey',
+  help:    'cyan',
+  warn:    'yellow',
+  debug:   'blue',
+  error:   'red'
+});
+
+var displayError = function(error) {
+  // Initial building up of the error
+  var errorString = '[' + error.plugin.error.bold + ']';
+  errorString += ' ' + error.message.replace("\n",''); // Removes new line at the end
+
+  // If the error contains the filename or line number add it to the string
+  if(error.fileName)
+      errorString += ' in ' + error.fileName;
+
+  if(error.lineNumber)
+      errorString += ' on line ' + error.lineNumber.bold;
+
+  // This will output an error like the following:
+  // [gulp-sass] error message in file_name on line 1
+  console.error(errorString);
+}
+
+var onError = function(err) {
+  notify.onError({
+    title:    "Gulp",
+    subtitle: "Failure!",
+    message:  "Error: <%= error.message %>",
+    sound:    "Basso"
+  })(err);
+  this.emit('end');
+};
+
+var sassOptions = {
+  outputStyle: 'expanded'
+};
+
+var prefixerOptions = {
+  browsers: ['last 2 versions']
+};
+
+// BUILD SUBTASKS
+// ---------------
+
+gulp.task('clean:dist', function() {
+  return gulp.src(bases.dist)
+    .pipe(vinylPaths(del));
+});
+
+
+gulp.task('styles', function() {
+  return gulp.src(bases.app + 'scss/default.scss')
+    .pipe(plumber({errorHandler: onError}))
+    .pipe(sourcemaps.init())
+    .pipe(sass(sassOptions))
+    .pipe(prefix(prefixerOptions))    
+    .pipe(rename('default.css'))
+    .pipe(sourcemaps.write('maps'))
+    .pipe(gulp.dest(bases.dist + 'css'))
+    .pipe(reload({stream:true}))    
+});
+
+gulp.task('themes', function() {
+  return gulp.src(bases.app + 'themes/*.scss')
+    .pipe(plumber({errorHandler: onError}))
+    .pipe(sourcemaps.init())
+    .pipe(sass(sassOptions))
+    .pipe(prefix(prefixerOptions))
+    .pipe(sourcemaps.write('maps'))
+    .pipe(gulp.dest(bases.dist + 'css'))
+    .pipe(reload({stream:true}))
+});
+
+gulp.task('styles:build', function() {
+  return gulp.src(bases.dist + 'css/*.css')
+    .pipe(cleanCSS())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest(bases.dist + 'css/min'))
+});
+
+gulp.task('browser-sync', function() {
+  browserSync({
+    server: {
+      baseDir: bases.dist
+    }
+  });
+});
+
+gulp.task('deploy', function() {
+  return gulp.src(bases.dist)
+    .pipe(deploy());
+});
+
+gulp.task('copy', function() {
+
+gulp.src(bases.app + 'img/*')
+    .pipe(gulp.dest(bases.dist + 'img'))
+    .pipe(reload({stream:true}));
+  
+    // copy icons to dist directly
+  gulp.src(bases.app + 'fonts/**/*.*')
+    .pipe(gulp.dest(bases.dist + 'fonts'))
+    .pipe(reload({stream:true}));
+
+    // copy icons to dist directly
+  gulp.src(bases.app + 'scss/**/*.*')
+    .pipe(gulp.dest(bases.dist + 'scss'))
+    .pipe(reload({stream:true}));    
+
+  gulp.src(bases.app + 'themes/*.*')
+    .pipe(gulp.dest(bases.dist + 'themes'))
+    .pipe(reload({stream:true}));      
+});
+
+gulp.task('lint', function() {
+  return gulp.src('./src/scss/**/*.scss')
+    .pipe(stylelint({
+      failAfterError: true,
+      reportOutputDir: 'reports/lint',
+      reporters: [
+        {formatter: 'verbose', console: true},
+        {formatter: 'json', save: 'lint-report.json'}
+      ],
+      debug: true
+    }));
+});
+
+
+gulp.task('html', function() {
+  gulp.src(bases.app + './*.html')
+    .pipe(gulp.dest(bases.dist))
+    .pipe(reload({stream:true}));
+});
+
+gulp.task('watch', function() {
+  gulp.watch(bases.app + 'scss/**/*.scss', ['styles']);
+  gulp.watch(bases.app + 'themes/*.scss', ['styles']);
+  gulp.watch(bases.app + './*.html', ['html']);
+  gulp.watch(bases.app + 'img/*', ['img']);
+});
+
+
+gulp.task('prompt', function () {
+  return gulp.src('')
+  .pipe(prompt.prompt({
+        type: 'input',
+        message: 'What would you like to name your new theme?',
+        name: 'themename',
+        default: 'demo'
+      },
+       function(response){
+
+        themename = response.themename;
+        
+    })
+  )
+})
+
+gulp.task('create-theme', function(){
+
+  if (fileExists(`src/themes/${themename}.scss`))
+    throw 'Theme with that name already exists'
+
+  return gulp.src('src/themes/bd.scss')
+    .pipe(rename(`${themename}.scss`))
+    .pipe(gulp.dest('src/themes', {overwrite: false}))
+})
+
+
+gulp.task('theme', function(done) {
+   runSequence('prompt', 'create-theme', 'default', done);
+});
+
+// BUILD TASKS
+// ------------
+
+gulp.task('default', function(done) {
+  runSequence('clean:dist', 'html', 'styles', 'themes', 'copy', 'styles:build', 'browser-sync', 'watch', done);
+});
+
+gulp.task('build', function(done) {
+  runSequence('clean:dist', 'html', 'styles', 'themes', 'copy', 'styles:build', done);
+});
