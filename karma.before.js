ae5daf68aacfa85dac5a948012061741c707c358
Maryna Semeniuk <Maryna_Semeniuk@epam.com>
2018-11-15 18:19:33 +0200

    improved toRgb: added alpha channel parameter

--- karma.before.js
+++ karma.before.js
@@ -1,17 +1,23 @@
-window.toRgb = function (hex) {
-    hex = hex.replace(/^#/, '');
-    let num = parseInt(hex, 16);
-    let RGB = [num >> 16, num >> 8 & 255, num & 255];
-    return 'rgb(' + RGB[0] + ', ' + RGB[1] + ', ' + RGB[2] + ')';
+window.toRgb = function (hex, alpha) {
+  hex = hex.replace(/^#/, '');
+  let num = parseInt(hex, 16);
+  let RGB = [num >> 16, num >> 8 & 255, num & 255];
+
+  if (arguments.length > 1) {
+    return 'rgba(' + RGB[0] + ', ' + RGB[1] + ', ' + RGB[2] + ', ' + alpha + ')';
+  }
+
+  return 'rgb(' + RGB[0] + ', ' + RGB[1] + ', ' + RGB[2] + ')';
 }
 
 window.$testContainer = $('<div class="c-body-text" data-test-container></div>');
 
 beforeEach((done) => {
-    $('body').append($testContainer);
-    done();
+  $('body').append($testContainer);
+  done();
 });
 
 afterEach((done) => {
-    done();
+  done();
 });
+
