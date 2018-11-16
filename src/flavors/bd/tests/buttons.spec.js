0c4e9e9fd5922a8c405cc6eee01f7db205596268
Maryna Semeniuk <Maryna_Semeniuk@epam.com>
2018-11-16 14:57:18 +0200

    disabled buttons & buttons sizes tests

--- src/flavors/bd/tests/buttons.spec.js
+++ src/flavors/bd/tests/buttons.spec.js
@@ -17,25 +17,6 @@ describe('BD Buttons', () => {
     });
   });
 
-  // c-btn c-btn-primary
-  describe('BD Primary Button', () => {
-    let $primaryBtn = $('<button class="c-btn c-btn-primary">Button</button>');
-    let $primaryBgImg = 'linear-gradient(' + toRgb('#008ddc') + ', ' + toRgb('#0079bd') + ' 66%, ' + toRgb('#006ca9') + ')';
-
-    before((done) => {
-      $testContainer.append($primaryBtn);
-      done();
-    });
-
-    it('should have correct styles', () => {
-      expect($primaryBtn.css('height'), 'button height').to.equal('30px');
-      expect($primaryBtn.css('color'), 'text-color').to.equal(toRgb('#ffffff'));
-      expect($primaryBtn.css('background-color'), 'background').to.equal(toRgb('#0079bd'))
-      expect($primaryBtn.css('background-image'), 'background-image').to.equal($primaryBgImg);
-      expect($primaryBtn.css('border'), 'border').to.equal('1px solid rgba(0, 0, 0, 0)');
-    });
-  });
-
   // c-btn c-btn-default
   describe('BD Default Button', () => {
     let $defaultBtn = $('<button class="c-btn c-btn-default">Button</button>');
@@ -55,62 +36,6 @@ describe('BD Buttons', () => {
     });
   });
 
-  // c-btn c-btn-success
-  describe('BD Success button', () => {
-    let $successBtn = $('<button class="c-btn c-btn-success">Button</button>');
-    let $successBgImg = 'linear-gradient(' + toRgb('#43c55a') + ', ' + toRgb('#37b24d') + ' 66%, ' + toRgb('#32a246') + ')';
-
-    before((done) => {
-      $testContainer.append($successBtn);
-      done();
-    });
-
-    it('should have correct styles', () => {
-      expect($successBtn.css('height'), 'button height').to.equal('30px');
-      expect($successBtn.css('color'), 'text color').to.equal(toRgb('#ffffff'));
-      expect($successBtn.css('background-color'), 'background').to.equal(toRgb('#37b24d'));
-      expect($successBtn.css('background-image'), 'background-image').to.equal($successBgImg);
-      expect($successBtn.css('border'), 'border').to.equal('1px solid rgba(0, 0, 0, 0)');
-    });
-  });
-
-  // c-btn c-btn-warning
-  describe('BD Warning button', () => {
-    let $warningBtn = $('<button class="c-btn c-btn-warning">Button</button>');
-    let $warningBgImg = 'linear-gradient(' + toRgb('#ff9b10') + ', ' + toRgb('#f08c00') + ' 66%, ' + toRgb('#dc8000') + ')';
-    before((done) => {
-      $testContainer.append($warningBtn);
-      done();
-    });
-
-    it('should have correct styles', () => {
-      expect($warningBtn.css('height'), 'button height').to.equal('30px');
-      expect($warningBtn.css('color'), 'text color').to.equal(toRgb('#ffffff'));
-      expect($warningBtn.css('background-color'), 'background').to.equal(toRgb('#f08c00'));
-      expect($warningBtn.css('background-image'), 'background-image').to.equal($warningBgImg);
-      expect($warningBtn.css('border'), 'border').to.equal('1px solid rgba(0, 0, 0, 0)');
-    });
-  });
-
-  // c-btn c-btn-danger
-  describe('BD Danger Button', () => {
-    let $dangerBtn = $('<button class="c-btn c-btn-danger">Button</button>');
-    let $dangerBgImg = 'linear-gradient(' + toRgb('#f25a5a') + ', ' + toRgb('#f03e3e') + ' 66%, ' + toRgb('#ef2b2b') + ')';
-
-    before((done) => {
-      $testContainer.append($dangerBtn);
-      done();
-    });
-
-    it('should have correct styles', () => {
-      expect($dangerBtn.css('height'), 'button height').to.equal('30px');
-      expect($dangerBtn.css('color'), 'text color').to.equal(toRgb('#ffffff'));
-      expect($dangerBtn.css('background-color'), 'background').to.equal(toRgb('#f03e3e'));
-      expect($dangerBtn.css('background-image'), 'background-image').to.equal($dangerBgImg);
-      expect($dangerBtn.css('border'), 'border').to.equal('1px solid ' + toRgb('#000000', 0));
-    });
-  });
-
   // c-btn c-btn-secondary
   describe('BD Secondary Button', () => {
     let $secondaryBtn = $('<button class="c-btn c-btn-secondary">Button</button>');
@@ -118,7 +43,6 @@ describe('BD Buttons', () => {
 
     before((done) => {
       $testContainer.append($secondaryBtn);
-      $testContainer.append('<br>');
       done();
     });
 
@@ -131,6 +55,60 @@ describe('BD Buttons', () => {
     });
   });
 
+  // btn btn-* (primary, success, warning, danger)
+  describe('BD Primary, Success, Warning, Danger Button', ()=> {
+    let $btnStatuses = {
+      'primary': {
+        'background': toRgb('#0079bd'),
+        'backgroundImage': 'linear-gradient(' + toRgb('#008ddc') + ', ' +
+                                                toRgb('#0079bd') + ' 66%, ' +
+                                                toRgb('#006ca9') + ')'
+      },
+      'success': {
+        'background': toRgb('#37b24d'),
+        'backgroundImage': 'linear-gradient(' + toRgb('#43c55a') + ', ' +
+                                                toRgb('#37b24d') + ' 66%, ' +
+                                                toRgb('#32a246') + ')'
+      },
+      'warning': {
+        'background': toRgb('#f08c00'),
+        'backgroundImage': 'linear-gradient(' + toRgb('#ff9b10') + ', ' + 
+                                                toRgb('#f08c00') + ' 66%, ' + 
+                                                toRgb('#dc8000') + ')'
+      },
+      'danger': {
+        'background': toRgb('#f03e3e'),
+        'backgroundImage': 'linear-gradient(' + toRgb('#f25a5a') + ', ' + 
+                                                toRgb('#f03e3e') + ' 66%, ' + 
+                                                toRgb('#ef2b2b') + ')'
+      }
+    };
+
+    for (let $status in $btnStatuses) {
+      let $btn = $('<button class="c-btn c-btn-' + $status +'">Button</button>'),
+          $btnBg = $btnStatuses[$status],
+          $btnStatus = $status,
+          testParameters = [$btn, $btnBg, $btnStatus];
+
+      before((done) => {
+        $testContainer.append($btn);
+        done();
+      });
+
+      testButtons(...testParameters);
+    }
+
+    function testButtons($btn, $btnBg, $btnStatus) {
+      it($btnStatus + ' btn should have correct styles', () => {
+        expect($btn.css('height'), 'button height').to.equal('30px');
+        expect($btn.css('color'), 'text color').to.equal(toRgb('#ffffff'));
+        expect($btn.css('background-color'), 'background').to.equal($btnBg.background);
+        expect($btn.css('background-image'), 'background-image').to.equal($btnBg.backgroundImage);
+        expect($btn.css('border'), 'border').to.equal('1px solid rgba(0, 0, 0, 0)');
+      });
+    }
+  });
+
   // c-btn-primary, c-btn-success, c-btn-warning, c-btn-danger disabled
   describe('BD Primary, Success, Warning, Danger Button Disabled', () => {
 
@@ -139,13 +117,13 @@ describe('BD Buttons', () => {
       'success': toRgb('#37b24d', '0.5'),
       'warning': toRgb('#f08c00', '0.5'),
       'danger': toRgb('#f03e3e', '0.5')
-    }
+    };
 
-    for ($status in $btnColors) {
-      let $btnDisabled = $('<button class="c-btn c-btn-' + $status + '" disabled>Button</button>');
-      let $btnColorValue = $btnColors[$status];
-      let $btnStatus = $status;
-      let testParameters = [$btnDisabled, $btnColorValue, $btnStatus];
+    for (let $status in $btnColors) {
+      let $btnDisabled = $('<button class="c-btn c-btn-' + $status + '" disabled>Button</button>'),
+          $btnColorValue = $btnColors[$status],
+          $btnStatus = $status,
+          testParameters = [$btnDisabled, $btnColorValue, $btnStatus];
 
       before((done) => {
         $testContainer.append($btnDisabled);
@@ -156,7 +134,7 @@ describe('BD Buttons', () => {
     }
 
     function testDisabledButtons($btnDisabled, $btnColorValues, $btnStatus) {
-      it($btnStatus + ' should have correct styles', () => {
+      it($btnStatus + ' btn should have correct styles', () => {
         expect($btnDisabled.css('background-color'), 'background').to.equal($btnColorValues);
         expect($btnDisabled.css('color'), 'color').to.equal(toRgb('#f8f9fa'));
       });
@@ -178,6 +156,44 @@ describe('BD Buttons', () => {
       expect($defaultBtnDisabled.css('background-color'), 'background').to.equal(toRgb('#f8f9fa'));
       expect($defaultBtnDisabled.css('border'), 'border').to.equal('1px solid ' + toRgb('#ced4da'));
     });
+  });
 
+  // c-btn sizes xs, sm, lg
+  describe('BD Button Sizes ', () => {
+    let $btnSizes = {
+      'xs': {
+        'padding': '3px 7px',
+        'fontSize': '12px'
+      },
+      'sm': {
+        'padding': '5px 10px',
+        'fontSize': '13px'
+      },
+      'lg': {
+        'padding': '9px 19px',
+        'fontSize': '15px'
+      }
+    };
+
+    for (let $size in $btnSizes) {
+      let $btn = $('<button class="c-btn c-btn-' + $size + ' c-btn-primary c-m-right-md">' + $size + ' button</button>'),
+          $btnStyles = $btnSizes[$size],
+          $btnSize = $size,
+          testParameters = [$btn, $btnStyles, $btnSize];
+
+      before((done) => {
+        $testContainer.append($btn);
+        done();
+      });
+
+      testBtnSizes(...testParameters);
+    }
+
+    function testBtnSizes($btn, $btnStyles, $btnSize) {
+      it($btnSize + ' btn should have correct styles', () => {
+        expect($btn.css('font-size'), 'font size').to.equal($btnStyles.fontSize);
+        expect($btn.css('padding'), 'padding').to.equal($btnStyles.padding);
+      });
+    }
   });
 });
