56db69b042e7cb552a0023171b0c16940f27291d
Marharyta Poliakovska <Marharyta_Poliakovska@epam.com>
2019-07-05 14:41:33 +0300

    chore: tests for bd flavor's button groups

--- src/flavors/bd/tests/button-groups.spec.js
+++ src/flavors/bd/tests/button-groups.spec.js
@@ -1,6 +1,10 @@
 describe('BD Button Groups', () => {
-  describe('BD Button Group', () => {
-    let $defaultBtnGroup = $('<div class="c-btn-group"><button>Button</button></div>');
+  describe('BD Default Button Group', () => {
+    let $defaultBtnGroup = $(`<div class="c-btn-group">
+                                <button>Button</button>
+                                <button>Button</button>
+                                <button>Button</button>
+                              </div>`);
 
     before((done) => {
       $testContainer.append($defaultBtnGroup);
@@ -12,34 +16,22 @@ describe('BD Button Groups', () => {
       expect($defaultBtnGroup.css('vertical-align'), 'vertical-align').to.equal('middle');
     });
   });
-
-  describe('BD Button Primary Outline', () => {
-    let $primaryBtnOutline = $('<button class="c-btn c-btn-primary-outline">Button</button>');
+  
+  describe('BD Vertical Button Group', () => {
+    let $verticalBtnGroup = $(`<div class="c-btn-group-vertical">
+                                <button>Button</button>
+                                <button>Button</button>
+                                <button>Button</button>
+                              </div>`);
 
     before((done) => {
-      $testContainer.append($primaryBtnOutline);
+      $testContainer.append($verticalBtnGroup);
       done();
     });
 
     it('should have correct styles', () => {
-      expect($primaryBtnOutline.css('background-color'), 'background-color').to.equal('rgba(0, 0, 0, 0)');
-      expect($primaryBtnOutline.css('color'), 'color').to.equal(toRgb('#0079bd'));
-      expect($primaryBtnOutline.css('border'), 'border').to.equal('1px solid ' + toRgb('#0079bd'));
-      expect($primaryBtnOutline.css('box-shadow'), 'box-shadow').to.equal('none');
-    });
-  });
-
-  describe('BD Default Button Disabled', () => {
-    let $defaultBtnBlock = $('<button class="c-btn c-btn-block">Button</button>');
-
-    before((done) => {
-      $testContainer.append($defaultBtnBlock);
-      done();
-    });
-
-    it('should have correct styles', () => {
-      expect($defaultBtnBlock.css('display'), 'display').to.equal('block');
-      expect($defaultBtnBlock.css('text-align'), 'text-align').to.equal('center');
+      expect($verticalBtnGroup.css('display'), 'display').to.equal('inline-flex');
+      expect($verticalBtnGroup.css('flex-direction'), 'flex-direction').to.equal('column');
     });
   });
 });
