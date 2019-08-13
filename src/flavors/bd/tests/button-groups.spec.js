0849419f7c5bc9d8340960c48fb4ea255032cb1c
Marharyta Poliakovska <maryna_semeniuk@epam.com>
2019-08-13 18:15:34 +0300

    chore: add tests for bd flavor's button groups

--- src/flavors/bd/tests/button-groups.spec.js
+++ src/flavors/bd/tests/button-groups.spec.js
@@ -1,9 +1,9 @@
 describe('BD Button Groups', () => {
   describe('BD Default Button Group', () => {
     let $defaultBtnGroup = $(`<div class="c-btn-group">
-                                <button class="c-btn">Button</button>
-                                <button class="c-btn">Button</button>
-                                <button class="c-btn">Button</button>
+                                <button class="c-btn c-btn-primary">Button</button>
+                                <button class="c-btn c-btn-primary">Button</button>
+                                <button class="c-btn c-btn-primary">Button</button>
                               </div>`);
 
     before((done) => {
@@ -11,17 +11,22 @@ describe('BD Button Groups', () => {
       done();
     });
 
-    it('The first child should have correct styles', () => {
+    it('should have correct styles', () => {
+      expect($defaultBtnGroup.css('display'), 'display').to.equal('inline-flex');
+      expect($defaultBtnGroup.css('vertical-align'), 'vertical-align').to.equal('middle');
+
+      //c-btn-group c-btn
+      expect(window.getComputedStyle(document.querySelector('.c-btn-group .c-btn')).getPropertyValue('position'), 'position').to.equal('relative');
+
+      //c-btn-group c-btn:first-child
       expect(window.getComputedStyle(document.querySelector('.c-btn-group .c-btn:first-child')).getPropertyValue('border-left-width'), 'border-left-width').to.equal('1px');
       expect(window.getComputedStyle(document.querySelector('.c-btn-group .c-btn:first-child')).getPropertyValue('border-top-right-radius'), 'border-top-right-radius').to.equal('0px');
       expect(window.getComputedStyle(document.querySelector('.c-btn-group .c-btn:first-child')).getPropertyValue('border-bottom-right-radius'), 'border-bottom-right-radius').to.equal('0px');
-    });
 
-    it('Neither the first, nor the last child should have correct styles', () => {
+      //c-btn-group c-btn:not(:first-child):not(:last-child)
       expect(window.getComputedStyle(document.querySelector('.c-btn-group .c-btn:not(:first-child):not(:last-child)')).getPropertyValue('border-bottom-right-radius'), 'border-radius').to.equal('0px');
-    });
 
-    it('The last child should have correct styles', () => {
+      //c-btn-group c-btn:last-child
       expect(window.getComputedStyle(document.querySelector('.c-btn-group .c-btn:last-child')).getPropertyValue('border-left-width'), 'border-left-width').to.equal('0px');
       expect(window.getComputedStyle(document.querySelector('.c-btn-group .c-btn:last-child')).getPropertyValue('border-top-left-radius'), 'border-top-left-radius').to.equal('0px');
       expect(window.getComputedStyle(document.querySelector('.c-btn-group .c-btn:last-child')).getPropertyValue('border-bottom-left-radius'), 'border-bottom-left-radius').to.equal('0px');
@@ -30,9 +35,9 @@ describe('BD Button Groups', () => {
   
   describe('BD Vertical Button Group', () => {
     let $verticalBtnGroup = $(`<div class="c-btn-group-vertical">
-                                <button class="c-btn">Button</button>
-                                <button class="c-btn">Button</button>
-                                <button class="c-btn">Button</button>
+                                <button class="c-btn c-btn-primary">Button</button>
+                                <button class="c-btn c-btn-primary">Button</button>
+                                <button class="c-btn c-btn-primary">Button</button>
                               </div>`);
 
     before((done) => {
@@ -43,22 +48,17 @@ describe('BD Button Groups', () => {
     it('should have correct styles', () => {
       expect($verticalBtnGroup.css('display'), 'display').to.equal('inline-flex');
       expect($verticalBtnGroup.css('flex-direction'), 'flex-direction').to.equal('column');
-    });
 
-    it('The first, but not only child should have correct styles', () => {
+      //c-btn-group-vertical c-btn:first-child:not(:only-child)
       expect(window.getComputedStyle(document.querySelector('.c-btn-group-vertical .c-btn:first-child:not(:only-child)')).getPropertyValue('margin-top'), 'margin-top').to.equal('0px');
-    });
-
-    it('The first child should have correct styles', () => {
       expect(window.getComputedStyle(document.querySelector('.c-btn-group-vertical .c-btn:first-child:not(:only-child)')).getPropertyValue('border-bottom-right-radius'), 'border-bottom-right-radius').to.equal('0px');
       expect(window.getComputedStyle(document.querySelector('.c-btn-group-vertical .c-btn:first-child:not(:only-child)')).getPropertyValue('border-bottom-left-radius'), 'border-bottom-left-radius').to.equal('0px');
-    });
 
-    it('Neither the first, nor the last child should have correct styles', () => {
-      expect(window.getComputedStyle(document.querySelector('.c-btn-group-vertical .c-btn:not(:first-child):not(:last-child)')).getPropertyValue('border-bottom-right-radius'), 'border-radius').to.equal('0px');
-    });
+      //c-btn-group-vertical c-btn:not(:first-child):not(:last-child)
+      expect(window.getComputedStyle(document.querySelector('.c-btn-group-vertical .c-btn:not(:first-child):not(:last-child)')).getPropertyValue('border-radius'), 'border-radius').to.equal('0px');
 
-    it('The last child should have correct styles', () => {
+
+      //c-btn-group-vertical c-btn:last-child:not(:only-child)
       expect(window.getComputedStyle(document.querySelector('.c-btn-group-vertical .c-btn:last-child:not(:only-child)')).getPropertyValue('border-top-right-radius'), 'border-top-right-radius').to.equal('0px');
       expect(window.getComputedStyle(document.querySelector('.c-btn-group-vertical .c-btn:last-child:not(:only-child)')).getPropertyValue('border-top-left-radius'), 'border-top-left-radius').to.equal('0px');
     });
